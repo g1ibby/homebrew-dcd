4107c909242b3e184807ee915a6b1e8d9dd87c0f
GitHub Action <action@github.com>
2025-05-30 04:29:52 +0000

    Update DCD formula to v0.2.3

--- Formula/dcd.rb
+++ Formula/dcd.rb
@@ -5,13 +5,13 @@ class Dcd < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/g1ibby/dcd/releases/download/v__VERSION__/dcd-x86_64-apple-darwin.tar.gz"
-      sha256 "__SHA256_INTEL__"
+      url "https://github.com/g1ibby/dcd/releases/download/v0.2.3/dcd-x86_64-apple-darwin.tar.gz"
+      sha256 "d472ca0fc5bb77d4f84bfff617b54258579dcdb64c164e9a840824af66340d78"
     end
 
     on_arm do
-      url "https://github.com/g1ibby/dcd/releases/download/v__VERSION__/dcd-aarch64-apple-darwin.tar.gz"
-      sha256 "__SHA256_ARM__"
+      url "https://github.com/g1ibby/dcd/releases/download/v0.2.3/dcd-aarch64-apple-darwin.tar.gz"
+      sha256 "393d9ef83145d0d947ce7c18a3564233740c9678f13b1f23a15502b053523cf4"
     end
   end
 
